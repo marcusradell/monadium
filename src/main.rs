a8d626873234a139a5f51e818b3b3f035c598b6b
Marcus Rådell <marcus@radell.net>
2023-05-07 20:33:31 +0200

    refactor elements into components

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,46 @@
+#![allow(non_snake_case)]
+
 use axum::{response::Html, routing::get, Router};
 use dioxus::prelude::*;
 
+pub fn HeroHeader(cx: Scope) -> Element {
+    cx.render(rsx!(h1 {"Monadium.org"}))
+}
+
+pub fn PageHeader(cx: Scope) -> Element {
+    cx.render(rsx!(h2 {"Learning programming fundamentals with Rust"}))
+}
+
+pub fn ChapterHeader(cx: Scope) -> Element {
+    cx.render(rsx!(h3 {"Challenge 1"}))
+}
+
+pub fn ParagraphHeader(cx: Scope) -> Element {
+    cx.render(rsx!(p {"Run code with Rust"}))
+}
+
+#[derive(PartialEq, Props)]
+pub struct CodeContentProps {
+    pub content: String,
+}
+
+pub fn CodeContent(cx: Scope<CodeContentProps>) -> Element {
+    cx.render(rsx!(pre {"{cx.props.content}"}))
+}
+
 async fn app_endpoint() -> Html<String> {
     fn app(cx: Scope) -> Element {
-        let code_content = "fn main() {
+        let content = "fn main() {
     println!(&quot; Hello, world!&quot;); 
-}";
+}"
+        .to_string();
+
         cx.render(rsx!(div {
-            h1 {"Monadium.org"}
-            h2 {"Learning programming fundamentals with Rust"}
-            h3 {"Challenge 1"}
-            p {"Run code with Rust"}
-            pre {code_content}}))
+            HeroHeader {}
+            PageHeader {}
+            ChapterHeader {}
+            ParagraphHeader {}
+            CodeContent{content: content}}))
     }
 
     let mut app = VirtualDom::new(app);
