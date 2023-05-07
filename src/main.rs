d3cc357b992db0e0495cc9e0e18f23b88e53f46f
Marcus Rådell <marcus@radell.net>
2023-05-07 19:59:11 +0200

    add content to page

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,37 @@
-use axum::{routing::get, Router};
+use axum::{response::Html, routing::get, Router};
+use dioxus::prelude::*;
+
+async fn app_endpoint() -> Html<String> {
+    fn app(cx: Scope) -> Element {
+        let code_content = "fn main() {
+    println!(&quot; Hello, world!&quot;); 
+}";
+        cx.render(rsx!(div {
+            h1 {"Monadium.org"}
+            h2 {"Learning programming fundamentals with Rust"}
+            h3 {"Challenge 1"}
+            p {"Run code with Rust"}
+            pre {code_content}}))
+    }
+
+    let mut app = VirtualDom::new(app);
+
+    let _ = app.rebuild();
+
+    Html(dioxus_ssr::render(&app))
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let addr = std::net::SocketAddr::from(([0, 0, 0, 0], 3000));
+    println!("listening on http://{}", addr);
 
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
-        .serve(app.into_make_service())
+    axum::Server::bind(&addr)
+        .serve(
+            Router::new()
+                .route("/", get(app_endpoint))
+                .into_make_service(),
+        )
         .await
         .unwrap();
 }
