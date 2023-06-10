5f6986e24dd8d1b2a647a9e9867fa0b0a1351cf3
Marcus Rådell <marcus@radell.net>
2023-06-10 18:24:33 +0200

    move address to where it's used

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,6 @@ mod web;
 
 #[tokio::main]
 async fn main() {
-    let addr = std::net::SocketAddr::from(([0, 0, 0, 0], 3000));
-
     let courses_kit = kits::Courses::new();
     let modules_kit = kits::Modules::new();
     let challenges_kit = kits::Challenges::new();
@@ -26,9 +24,11 @@ async fn main() {
         .nest("/api", api_router)
         .nest("/", web_kit.router());
 
-    println!("Starting server on {}", addr);
+    let address = std::net::SocketAddr::from(([0, 0, 0, 0], 3000));
 
-    axum::Server::bind(&addr)
+    println!("Starting server on {}", address);
+
+    axum::Server::bind(&address)
         .serve(app_router.into_make_service())
         .await
         .unwrap();
