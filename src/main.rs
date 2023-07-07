8ce59ec49e56183a9711809360541bd0e7f2d0c2
Marcus Rådell <marcus@radell.net>
2023-07-07 22:17:45 +0200

    remove commented code

--- src/main.rs
+++ src/main.rs
@@ -38,15 +38,7 @@ async fn shuttle(
 
     let app_router = setup(config).await;
 
-    // let address = std::net::SocketAddr::from(([0, 0, 0, 0], 3000));
-
-    // info!("Booting server on {}", address);
-
-    // let server = axum::Server::bind(&address).serve(app_router.into_make_service());
-
     info!("Server ready.");
-
-    // server.await.expect("Server failed.");
 
     Ok(app_router.into())
 }
