98dcef40d9eb2450475cf10c59d7d1e94eb3a144
Marcus Rådell <marcus@radell.net>
2023-07-07 21:20:36 +0200

    try migrate macro in prod

--- src/server/io/env.rs
+++
@@ -1,6 +0,0 @@
-use std::env;
-
-pub fn ensure_env(var_name: &str) -> String {
-    env::var(var_name)
-        .unwrap_or_else(|_| panic!("{}", &format!("Missing {} environment variable.", var_name)))
-}
