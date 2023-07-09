af70a9dfb9cea16093017b5f74528a372205c1b8
Marcus Rådell <marcus@radell.net>
2023-07-09 21:41:58 +0200

    setup web project according to fullstack guide

--- web/src/main.rs
+++ web/src/main.rs
@@ -1,9 +1,26 @@
-mod app;
-pub use app::*;
+// mod app;
+// pub use app::*;
 
-mod kits;
-pub mod primitives;
+// mod kits;
+// pub mod primitives;
+
+#![allow(non_snake_case)]
+// Import the Dioxus prelude to gain access to the `rsx!` macro and the `Scope` and `Element` types.
+use dioxus::prelude::*;
 
 fn main() {
-    println!("Hello, world!");
+    wasm_logger::init(wasm_logger::Config::default().module_prefix("web"));
+
+    log::info!("Message on my console");
+    // Launch the web application using the App component as the root.
+    dioxus_web::launch(App);
+}
+
+// Define a component that renders a div with the text "Hello, world!"
+fn App(cx: Scope) -> Element {
+    cx.render(rsx! {
+        div {
+            "Hello, Monadium!"
+        }
+    })
 }
